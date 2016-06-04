16dda02ba928207f5206f5a3dda18de4becdaae0
fll15@alu.ua.es <fll15@alu.ua.es>
2016-06-04 12:31:48 +0200

    Segundo commit con deteccion de volumen

---
+++ app/src/main/java/com/example/francisco/trial/CustomDialogFragment.java
@@ -0,0 +1,15 @@
+package com.example.francisco.trial;
+
+import android.support.v4.app.DialogFragment;
+import android.app.Dialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.TextView;
+
+/**
+ * Created by Francisco on 16/04/2016.
+ */
+
